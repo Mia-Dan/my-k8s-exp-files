feaab64612cac53a70b6b9995c4305a9b0cad233
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2024-06-23 19:27:20 +0800

    ver 0.0.0 | res

--- run.sh
+++ run.sh
@@ -31,7 +31,18 @@ tar zxvf apache-jmeter-5.6.3.tgz
 # run exp
 cd ~/my-k8s-exp-files/ && ls
 ~/apache-jmeter-5.6.3/bin/jmeter.sh -n -t "/root/my-k8s-exp-files/for-cpu.jmx"
+# summary =      5 in 00:00:10 =    0.5/s Avg:  5593 Min:  2021 Max:  9193 Err:     0 (0.00%)
+# 运行下一行docker后,
+# summary =      5 in 00:00:09 =    0.6/s Avg:  4880 Min:  1728 Max:  7930 Err:     0 (0.00%)
+# k8s情况下,如果输入错了端口号,反应会慢
 
+# sup-exp in docker, without k8s
+docker run -p 8084:8084 miadan/exp-run-cpu:0.0.0
+vim for-cpu.jmx # edit the url
+# summary =      5 in 00:00:14 =    0.4/s Avg:  8231 Min:  2771 Max: 13340 Err:     0 (0.00%)
+# summary =      5 in 00:00:12 =    0.4/s Avg:  7077 Min:  2676 Max: 11266 Err:     0 (0.00%)
+# docker情况下,如果输入错了端口号,会很快反应
+# summary =      5 in 00:00:01 =    5.9/s Avg:    29 Min:     4 Max:    66 Err:     5 (100.00%)
 
 #---
 #
